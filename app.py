dddf0281023e2da688a34c3473bb6a7d864f6559
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2024-08-28 21:41:23 +0100

    updating tag

--- app.py
+++ app.py
@@ -4,13 +4,8 @@ from streamlit_gtag import st_gtag
 
 st_gtag(
     key="gtag_send_event_a",
-    id="G-4XZZ9XXZ21",
-    event_name="app_main_page",
-    params={
-        "event_category": "test_category_a",
-        "event_label": "test_label_a",
-        "value": 97,
-    },
+    id="G-5PQ2K3S5JG",
+    event_name="app_main_page"
 )
 
 
