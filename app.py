949a7945e5e9172bf630c33262cbc0d6d9b4b069
Chiyun-Lee <chiyun.alberto.lee@gmail.com>
2024-08-28 22:41:55 +0100

    new injection

--- app.py
+++ app.py
@@ -1,19 +1,47 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
-from streamlit_gtag import st_gtag
-
-st_gtag(
-    key="gtag_send_event_a",
-    id="G-5PQ2K3S5JG",
-    event_name="app_main_page"
-)
-
-
-if st.button("hello"):
-    st.toast("hi")
+def inject_gtag(gtag_id, height=0, width=0, scrolling=False, display=True):
+    """
+    Injects Google Tag (gtag.js) into the Streamlit app.
     
-    st_gtag(
-        key="gtag_send_event_b",
-        id="G-HXSDMRLF54",
-        event_name="app_main_page_b"
+    Parameters:
+        gtag_id (str): Your Google Tag Manager ID (e.g., 'G-XXXXXXXXXX').
+        height (int): The height of the HTML component. Default is 0 to minimize space usage.
+        width (int): The width of the HTML component. Default is 0 to minimize space usage.
+        scrolling (bool): Whether to allow scrolling within the HTML component. Default is False.
+        display (bool): If False, the script will not be injected. Default is True.
+    """
+    if not display:
+        return  # Skip injection if display is set to False
+
+    components.html(
+        f'''
+        <script>
+          // Create the script tag
+          var gtagScript = document.createElement('script');
+          gtagScript.async = true;
+          gtagScript.src = "https://www.googletagmanager.com/gtag/js?id={gtag_id}";
+          document.head.appendChild(gtagScript);
+
+          // Initialize Google Analytics
+          gtagScript.onload = function() {{
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){{dataLayer.push(arguments);}}
+            gtag('js', new Date());
+            gtag('config', '{gtag_id}');
+          }};
+        </script>
+        ''',
+        height=height,
+        width=width,
+        scrolling=scrolling
     )
+
+# Example usage:
+inject_gtag('G-5PQ2K3S5JG')
+
+
+
+if st.button("toast"):
+    st.toast("toast")
--- streamlit_gtag/frontend/main.js
+++ streamlit_gtag/frontend/main.js
@@ -1,67 +1,147 @@
+// class TagManager {
+//     constructor(id) {
+//       this.id = id;
+//     }
+  
+//     isTagManagerRegistered() {
+//       return [...window.parent.document.head.querySelectorAll("script")]
+//         .map((el) => {
+//           return el.src.includes(this.id);
+//         })
+//         .some((el) => {
+//           return el === true;
+//         });
+//     }
+  
+//     async setup() {
+//       if (this.isTagManagerRegistered()) return;
+  
+//       const ga = document.createElement("script");
+//       ga.async = true;
+//       ga.src = `https://www.googletagmanager.com/gtag/js?id=${this.id}`;
+  
+//       window.parent.document.head.insertBefore(
+//         ga,
+//         window.parent.document.head.firstChild
+//       );
+  
+//       const gtag = document.createElement("script");
+//       gtag.innerHTML = `
+//         window.dataLayer = window.dataLayer || [];
+        
+//         function gtag() {
+//           dataLayer.push(arguments);
+//         }
+  
+//         window.addEventListener("message", function(event) {
+//             if (event.data.source === "streamlit-ga") {
+//               gtag("event", event.data.eventName, event.data.eventData)
+//             }
+//           }
+//         )
+  
+//         gtag("js", new Date());
+//         gtag("config", '${this.id}');
+  
+//       `;
+//       window.parent.document.head.insertBefore(gtag, ga.nextSibling);
+//     }
+  
+//     sendEvent(eventName, params) {
+//       window.parent.postMessage({
+//         source: "streamlit-ga",
+//         eventName: eventName,
+//         eventData: params,
+//       });
+//     }
+//   }
+  
+//   async function onRender(event) {
+//     const props = event.detail.args;
+//     const tagManager = new TagManager(props.id);
+//     await tagManager.setup();
+//     await tagManager.sendEvent(props.event_name, props.params);
+//   }
+  
+//   Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
+//   Streamlit.setComponentReady();
+
+
 class TagManager {
-    constructor(id) {
+  constructor(id) {
       this.id = id;
-    }
-  
-    isTagManagerRegistered() {
+  }
+
+  isTagManagerRegistered() {
       return [...window.parent.document.head.querySelectorAll("script")]
-        .map((el) => {
-          return el.src.includes(this.id);
-        })
-        .some((el) => {
-          return el === true;
-        });
-    }
-  
-    async setup() {
+          .map((el) => el.src.includes(this.id))
+          .some((el) => el === true);
+  }
+
+  async setup() {
       if (this.isTagManagerRegistered()) return;
-  
+
       const ga = document.createElement("script");
       ga.async = true;
       ga.src = `https://www.googletagmanager.com/gtag/js?id=${this.id}`;
-  
-      window.parent.document.head.insertBefore(
-        ga,
-        window.parent.document.head.firstChild
-      );
-  
+
+      window.parent.document.head.insertBefore(ga, window.parent.document.head.firstChild);
+
       const gtag = document.createElement("script");
       gtag.innerHTML = `
-        window.dataLayer = window.dataLayer || [];
-        
-        function gtag() {
-          dataLayer.push(arguments);
-        }
-  
-        window.addEventListener("message", function(event) {
-            if (event.data.source === "streamlit-ga") {
-              gtag("event", event.data.eventName, event.data.eventData)
-            }
+          window.dataLayer = window.dataLayer || [];
+          
+          function gtag() {
+              dataLayer.push(arguments);
           }
-        )
-  
-        gtag("js", new Date());
-        gtag("config", '${this.id}');
-  
+
+          window.addEventListener("message", function(event) {
+              if (event.data.source === "streamlit-ga") {
+                  if (getCookie('cookie_accepted') === 'true') {
+                      gtag("event", event.data.eventName, event.data.eventData)
+                  }
+              }
+          });
+
+          gtag("js", new Date());
+          gtag("config", '${this.id}');
       `;
       window.parent.document.head.insertBefore(gtag, ga.nextSibling);
-    }
-  
-    sendEvent(eventName, params) {
+  }
+
+  sendEvent(eventName, params) {
       window.parent.postMessage({
-        source: "streamlit-ga",
-        eventName: eventName,
-        eventData: params,
+          source: "streamlit-ga",
+          eventName: eventName,
+          eventData: params,
       });
-    }
   }
-  
-  async function onRender(event) {
-    const props = event.detail.args;
-    const tagManager = new TagManager(props.id);
-    await tagManager.setup();
-    await tagManager.sendEvent(props.event_name, props.params);
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+  const expires = new Date(Date.now() + days * 864e5).toUTCString();
+  document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
+}
+
+// Function to get a cookie value
+function getCookie(name) {
+  return document.cookie.split('; ').reduce((r, v) => {
+      const parts = v.split('=');
+      return parts[0] === name ? decodeURIComponent(parts[1]) : r
+  }, '');
+}
+
+async function onRender(event) {
+  const props = event.detail.args;
+  const tagManager = new TagManager(props.id);
+  await tagManager.setup();
+
+  // Only send events if cookies have been accepted
+  if (getCookie('cookie_accepted') === 'true') {
+      await tagManager.sendEvent(props.event_name, props.params);
   }
-  
-  Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
-  Streamlit.setComponentReady();
+}
+
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
+Streamlit.setComponentReady();
